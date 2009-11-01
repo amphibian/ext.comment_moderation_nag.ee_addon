fced0c32d94cca965d67f8329a50ea676c49afb5
Derek Hogue <derek@amphibian.info>
2009-11-01 12:11:19 -0400

    Added language file; now checking for existence of comment module; code cleanup

--- extensions/ext.comment_moderation_nag.php
+++ extensions/ext.comment_moderation_nag.php
@@ -9,7 +9,7 @@ class Comment_moderation_nag
 {
 	var $settings        = array();
 	var $name            = 'Comment Moderation Nag';
-	var $version         = '1.0.1';
+	var $version         = '1.0.2';
 	var $description     = 'Prominently nags administrators to approve or delete pending comments.';
 	var $settings_exist  = 'n';
 	var $docs_url        = '';
@@ -30,39 +30,49 @@ class Comment_moderation_nag
 	{
 		global $EXT, $DB, $PREFS, $LANG, $DSP;
 		
-		if ($EXT->last_call !== FALSE)
+		if($EXT->last_call !== FALSE)
 		{
 			$out = $EXT->last_call;
 		}
 		
-		if(isset($_GET['M']) && $_GET['M'] == 'view_comments' 
-			&& isset($_GET['validate']) && $_GET['validate'] == 1)
-			return $out;
-		
-		if ($DSP->allowed_group('can_moderate_comments'))
+		// First, check to see if we have permission to moderate comments
+		if($DSP->allowed_group('can_moderate_comments'))
 		{
-			$query = $DB->query("
-				SELECT count(comment_id) AS count 
-				FROM exp_comments 
-				WHERE status = 'c' 
-				AND site_id = '".$DB->escape_str($PREFS->ini('site_id')) ."'
-			");
-			$total = $query->row['count'];
-			
-			if($total)
-			{	
-				$c_word = ($total == 1) ? ' comment requires' : ' comments require';
-				$find= "<div id='contentNB'>";
-				$replace = "
-				<div id='contentNB'>
-				<div class='box'>
-					<span class='highlight_bold'>".$total.$c_word." moderation. </span>
-					<a href='".BASE.AMP."C=edit&amp;M=view_comments&amp;validate=1'>
-					Click here to approve or delete pending comments.</a>
-					</span>
-				</div>
-				";
-				$out = str_replace($find, $replace, $out);
+			// Next, make sure we're not already on the moderation screen
+			if(isset($_GET['M']) && $_GET['M'] == 'view_comments' 
+				&& isset($_GET['validate']) && $_GET['validate'] == 1)
+				return $out;
+				
+			// Only proceed if the Comment module is actually insatlled
+			$module_check = $DB->query("SELECT module_name FROM exp_modules WHERE module_name = 'Comment'");			
+			if($module_check->num_rows > 0)
+			{
+				$query = $DB->query("
+					SELECT count(comment_id) AS count 
+					FROM exp_comments 
+					WHERE status = 'c' 
+					AND site_id = '".$DB->escape_str($PREFS->ini('site_id')) ."'
+				");
+				$total = $query->row['count'];
+				
+				// Lastly, only display the notice if there are comments to moderate
+				if($total)
+				{	
+					$LANG->fetch_language_file('comment_moderation_nag');
+					$c_word = ($total == 1) ? $LANG->line('comment_requires') : $LANG->line('comments_require');
+					$find= "<div id='contentNB'>";
+					$replace = "
+					<div id='contentNB'>
+					<div class='box'>
+						<span class='highlight_bold'>".$total." ".$c_word." ".$LANG->line('moderation').". </span>
+						<a href='".BASE.AMP."C=edit&amp;M=view_comments&amp;validate=1'>".
+						$LANG->line('click_to_moderate').
+						".</a>
+						</span>
+					</div>
+					";
+					$out = str_replace($find, $replace, $out);
+				}
 			}
 		}
 		return $out;
@@ -90,7 +100,7 @@ class Comment_moderation_nag
 			        'class'        => "Comment_moderation_nag",
 			        'method'       => $method,
 			        'hook'         => $hook,
-			        'settings'     => serialize($defaults),
+			        'settings'     => '',
 			        'priority'     => 10,
 			        'version'      => $this->version,
 			        'enabled'      => "y"
@@ -115,10 +125,6 @@ class Comment_moderation_nag
 	    {
 	        return FALSE;
 	    }
-	    
-	    if ($current < '1.0.1')
-	    {
-		}
 	    
 	    $DB->query("UPDATE exp_extensions 
 	                SET version = '".$DB->escape_str($this->version)."' 
---
+++ language/english/lang.comment_moderation_nag.php
@@ -0,0 +1,10 @@
+<?php
+
+global $PREFS;
+
+$L = array(
+	'comments_require' => 'comments require',
+	'comment_requires' => 'comment requires',
+	'moderation' => 'moderation',
+	'click_to_moderate' => 'Click here to approve or delete pending comments'
+);
