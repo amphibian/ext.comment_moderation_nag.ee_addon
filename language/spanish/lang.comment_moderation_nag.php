3227d6f532378547577b77b32fdd9791f56fbe70
Derek Hogue <derek@amphibian.info>
2009-11-02 16:50:22 -0400

    Added Spanish language file (thanks to Nelly Mercado.)

---
+++ language/spanish/lang.comment_moderation_nag.php
@@ -0,0 +1,10 @@
+<?php
+
+global $PREFS;
+
+$L = array(
+	'comments_require' => 'comentarios por',
+	'comment_requires' => 'comentario por',
+	'moderation' => 'moderar',
+	'click_to_moderate' => 'De click aquí para aprobar o borrar los comentarios pendientes'
+);
